c127c53f029aa5fb06d4af7b8ac4fc8521a798e5
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-09-07 17:04:14 +0200

    Main side-typ klar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
-
       .then((res) => setPosts(res.posts));
+
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((res) => setUsers(res.users));
   }, []);
+
+  const usersMap = users.reduce((map, user) => {
+    map[user.id] = user;
+    return map;
+  }, {});
 
   return (
     <>
       {posts.map((post, index) => (
-        <div class="EachPost" key={index}>
-          <h3 className="PostTitle">{post.title}</h3>
-          <p className="PostBody">{post.body.slice(0, 60)}...</p>
-          <label className="PostTags">
+        <div key={index}>
+          <h3>{post.title}</h3>
+          {usersMap[post.userId] && (
+            <p className="userName">{usersMap[post.userId].username} </p>
+          )}
+          <p>{post.body.slice(0, 60)}...</p>
+          <label>
             {post.tags.map((tag, tagIndex) => (
               <span key={tagIndex}>
                 {tag}

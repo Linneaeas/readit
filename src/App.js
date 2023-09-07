31df0b05cd366924f2217aa301aa0d4e3d5d4b79
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-09-07 16:16:15 +0200

    Posts, Title, Body(length), Tags

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import "./App.css";
-import { MainPage } from "./Views/main-page";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+
+      .then((res) => setPosts(res.posts));
+  }, []);
+
   return (
-    <div className="App">
-      <p>Test</p>
-    </div>
+    <>
+      {posts.map((post, index) => (
+        <div class="EachPost" key={index}>
+          <h3 className="PostTitle">{post.title}</h3>
+          <p className="PostBody">{post.body.slice(0, 60)}...</p>
+          <label className="PostTags">
+            {post.tags.map((tag, tagIndex) => (
+              <span key={tagIndex}>
+                {tag}
+                {tagIndex < post.tags.length - 1 && " "}
+              </span>
+            ))}
+          </label>
+        </div>
+      ))}
+    </>
   );
 }
 
